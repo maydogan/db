7b7498f2e8597006315b5d24e535365cb83c5cba
maydogan <meltem.aydogan@bil.omu.edu.tr>
2013-03-23 18:24:22 +0200

    .

---
+++ table/sales_order.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE sales_order
+( s_order_no varchar(6),
+  s_order_date date ,
+  client_no varchar(6),
+  dely_add varchar(6),
+  salesman_no varchar(6),
+  dely_type char(1),
+  billed_yn char(1), 
+  dely_date date,
+  order_status varchar(10),
+  CONSTRAINT cm_no_pk PRIMARY KEY(client_no) REFERENCES client_master (client_no),
+  CONSTRAINT sm_no_fk FOREIGN KEY(salesman_no) REFERENCES salesman_master(salesman_no),
+  CONSTRAINT "sales_order_check" CHECK (dely_date > s_order_date),
+  check (s_order_no like '0%');
+
+);
--- table/salesman_master.sql
+++ table/salesman_master.sql
@@ -1,14 +1,14 @@
 ﻿CREATE TABLE salesman_master
 ( salesman_no varchar(6) CONSTRAINT sm_no_fk primary key, 
-sal_name varchar(20) NOT NULL,
-address varchar(20)  NOT NULL,
-city varchar(20),
-state varchar(20),
-pincode numeric(6),
-sal_amt numeric(8,2) NOT NULL , 
-tgt_to_get numeric(6,2) NOT NULL ,
-ytd_sales numeric(6,2) NOT NULL ,
-remarks varchar (30),
-check (salesman_no like 'S%'),
-check (sal_amt != 0 and tgt_to_get != 0 and ytd_sales !=0)
+  sal_name varchar(20) NOT NULL,
+  address varchar(20)  NOT NULL,
+  city varchar(20),
+  state varchar(20),
+  pincode numeric(6),
+  sal_amt numeric(8,2) NOT NULL , 
+  tgt_to_get numeric(6,2) NOT NULL ,
+  ytd_sales numeric(6,2) NOT NULL ,
+  remarks varchar (30),
+  check (salesman_no like 'S%'),
+  check (sal_amt != 0 and tgt_to_get != 0 and ytd_sales !=0)
 );
