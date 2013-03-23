4ef4131d1dfb9235527289f3b0b37c0bd605d6d0
maydogan <meltem.aydogan@bil.omu.edu.tr>
2013-03-23 18:55:35 +0200

    .

--- table/sales_order.sql
+++ table/sales_order.sql
@@ -8,9 +8,9 @@
   billed_yn char(1), 
   dely_date date,
   order_status varchar(10),
-  CONSTRAINT cm_no_pk PRIMARY KEY(client_no) REFERENCES client_master (client_no),
-  CONSTRAINT sm_no_fk FOREIGN KEY(salesman_no) REFERENCES salesman_master(salesman_no),
+  CONSTRAINT pk_cmcreateso PRIMARY KEY (client_no) REFERENCES client_master (client_no),
+  CONSTRAINT fk_smcreateso FOREIGN KEY (salesman_no) REFERENCES salesman_master (salesman_no),
   CONSTRAINT "sales_order_check" CHECK (dely_date > s_order_date),
-  check (s_order_no like '0%');
+  check (s_order_no like '0%')
 
 );
