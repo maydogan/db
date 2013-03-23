b8075e70eaf65e8d21702f9dac401e08ef90cba9
maydogan <meltem.aydogan@bil.omu.edu.tr>
2013-03-23 21:06:56 +0200

    .

--- table/sales_order.sql
+++ table/sales_order.sql
@@ -1,16 +1,19 @@
 ﻿CREATE TABLE sales_order
 ( s_order_no varchar(6),
   s_order_date date ,
-  client_no varchar(6),
+  client_no varchar(25),
   dely_add varchar(6),
   salesman_no varchar(6),
-  dely_type char(1),
+  dely_type char(1) ,
   billed_yn char(1), 
   dely_date date,
   order_status varchar(10),
-  CONSTRAINT pk_cmcreateso PRIMARY KEY (client_no),
-  CONSTRAINT fk_smcreateso FOREIGN KEY (salesman_no) REFERENCES salesman_master (salesman_no),
-  CONSTRAINT "sales_order_check" CHECK (dely_date > s_order_date),
-  check (s_order_no like '0%')
+  CONSTRAINT pk_s_order_no PRIMARY KEY (s_order_no),
+  CONSTRAINT fk_client_no FOREIGN KEY (client_no) REFERENCES client_master (client_no),
+  CONSTRAINT fk_salesmen_no FOREIGN KEY (salesman_no) REFERENCES salesman_master (salesman_no),
+  check (dely_date > s_order_date),
+  check (s_order_no ~ '^0'),
+  check (order_status = ANY(array['Ip', 'F', 'Bo', 'C']))
+  
 
 );
--- table/sales_order_details.sql
+++ table/sales_order_details.sql
@@ -4,7 +4,7 @@
   qty_order numeric(8),
   qty_disp numeric(8),
   product_rate numeric(10,2),   
-  CONSTRAINT fk_cmcreatesod FOREIGN KEY (product_no) REFERENCES product_master (product_no),
-  CONSTRAINT fk_socreatesod FOREIGN KEY (s_order_no) REFERENCES sales_order (s_order_no)
+  CONSTRAINT fk_product_no FOREIGN KEY (product_no) REFERENCES product_master (product_no),
+  CONSTRAINT fk_s_order_no FOREIGN KEY (s_order_no) REFERENCES sales_order (s_order_no)
 
 );
