130b1f68c4fa82f56c515e6166f34e64eaed3938
maydogan <meltem.aydogan@bil.omu.edu.tr>
2013-03-23 17:43:44 +0200

    .

--- table/product_master.sql
+++ table/product_master.sql
@@ -1,11 +1,10 @@
- 
-CREATE TABLE client_master
-( product_no varchar() PRIMARY KEY,
-description varchar(),
-profit_percent numeric(),
-unit_measure varchar(),
-qty_on_hand numeric(),
-reorder_lvl numeric(),
-sell_price numeric(),
-cost_price numeric()
+ CREATE TABLE product_master
+( product_no varchar(6) PRIMARY KEY,
+description varchar(20),
+profit_percent numeric(20),
+unit_measure varchar(20),
+qty_on_hand numeric(20),
+reorder_lvl numeric(20),
+sell_price numeric(20),
+cost_price numeric(20)
 );
