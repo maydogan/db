d3a5b77f940809b5383c29ebd11b06f8a9451900
maydogan <meltem.aydogan@bil.omu.edu.tr>
2013-03-24 13:32:29 +0200

    .

--- data/sales_order.sql
+++ data/sales_order.sql
@@ -1,8 +1,8 @@
-﻿ insert into sales_order (s_order_no, s_order_date, client_no, dely_type, billed_yn, salesman_no, dely_date, order_status)
+﻿ insert into sales_order (s_order_no, s_order_date, client_no,salesman_no, dely_type, billed_yn,  dely_date, order_status)
 values
-    (019001, '12-jan-96', 0001, 'F', 'N', 'S00001', '20-jan-96', 'Ip'),
-    (019002, '25-jan-96', 0002, 'P', 'N', 'S00002', '27-jan-96', 'C'),
-    (016865, '18-feb-96', 0003, 'F', 'Y', 'S00003', '20-feb-96', 'F'),
-    (019003, '03-apr-96', 0001, 'F', 'Y', 'S00001', '07-apr-96', 'F'),
-    (046866, '20-may-96', 0004, 'P', 'N', 'S00002', '22-may-96', 'C'),
-    (010008, '24-may-96', 0005, 'F', 'N', 'S00004', '26-may-96', 'Ip');
+    (019001, '12-jan-96', 0001, 'S00001', 'F', 'N',  '20-jan-96', 'Ip'),
+    (019002, '25-jan-96', 0002, 'S00002', 'P', 'N',  '27-jan-96', 'C'),
+    (016003, '18-feb-96', 0003, 'S00003', 'F', 'Y',  '20-feb-96', 'F'),
+    (016865, '03-apr-96', 0001, 'S00001', 'F', 'Y',  '07-apr-96', 'F'),
+    (046866, '20-may-96', 0004, 'S00002', 'P', 'N',  '22-may-96', 'C'),
+    (010008, '24-may-96', 0005, 'S00004', 'F', 'N',  '26-may-96', 'Ip');
